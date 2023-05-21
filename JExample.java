19bb68a38b20a2ef01e545b36c228401ac31cac7
Kamapholoba <129863763+Kamapholoba@users.noreply.github.com>
2023-05-21 19:54:23 +0200

    Add files via upload

---
+++ JExample.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jexample;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Nathi Ngcobo
+ */
+public class JExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       String age = JOptionPane.showInputDialog(null, "Enter your age");
+      
+      JOptionPane.showMessageDialog(null, age);
+      
+      String flowerColour = JOptionPane.showInputDialog(null, "Enter your flower colour");
+      
+      JOptionPane.showMessageDialog(null, flowerColour);
+      
+        
+      
+      
+      
+      
+      
+      
+     
+        
+    }
+    
+}
